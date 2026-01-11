9124c83bb82fa66543aa08aa22f18c93bac4785c
MM1323 <mia.mcduffie.13@gmail.com>
2026-01-11 17:28:33 -0500

    Adding DemoGame class

---
+++ DemoGame.java
@@ -0,0 +1,6 @@
+public class DemoGame extends BaseGame {
+	// Minimal demo wrapper to satisfy GameLauncher when DEMO mode is enabled.
+	public DemoGame(int hdim, int wdim, int urow) {
+		super(hdim, wdim, urow);
+	}
+}
